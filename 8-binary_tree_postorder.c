fc9ceb1d84238b4822eab114b38ba9b206ff1bc1
kwamboka1 <makorimildred@gmail.com>
2022-02-18 10:43:54 +0300

    [Fix]: code

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -12,9 +12,8 @@
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree || !func)
-                return;
-
-        binary_tree_postorder(tree->left, func);
+		return;
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
 	func(tree->n);
-        binary_tree_postorder(tree->right, func);
 }
