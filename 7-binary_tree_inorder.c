8dd4d8dea0336980c788b9421a3395e3b24a53b5
kwamboka1 <makorimildred@gmail.com>
2022-02-18 10:34:16 +0300

    [Fix]: code

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -10,9 +10,9 @@
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL || func == NULL)
+	if (!tree || !func)
 		return;
 	binary_tree_inorder(tree->left, func);
-	func(tree->);
+	func(tree->n);
 	binary_tree_inorder(tree->right, func);
 }
